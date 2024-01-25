3af4f1338acbebabc82e0ccbe6244c3637136e53
Yuvraj3905 <yuvraj202001@gmail.com>
2024-01-25 13:36:27 +0530

    Understanding Map(loops)

--- P2/vite-project/src/component/FoodList.jsx
+++ P2/vite-project/src/component/FoodList.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 
 export default function FoodList(props) {
+    let item=props.item;
   return (
     <>
-    {console.log(props.item)}
     <div className='container'>
         <h2 className='p-3'>Food Items</h2>
     <ul className="list-group">
-  <li className="list-group-item">{props.item[0]}</li>
-  <li className="list-group-item">{props.item[1]}</li>
-  <li className="list-group-item">{props.item[2]}</li>
-  <li className="list-group-item">{props.item[3]}</li>
-  <li className="list-group-item">{props.item[4]}</li>
+        {item.map((x)=>(
+  <li className="list-group-item">{x}</li>
+        ))}
+  
 </ul>
-
     </div>
     </>
   )
