cfadb16ffa9d86f728b91716306adaf34433f20e
Yuvraj3905 <yuvraj202001@gmail.com>
2024-01-25 16:59:48 +0530

    Edited

--- P2/vite-project/src/component/FoodItem.jsx
+++ P2/vite-project/src/component/FoodItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import style from './css_module/FoodItem.module.css'
 export default function FoodItem({x}) {
   return (
    <>
-   <li  className="list-group-item l1">{x}</li>
+   <li  className={style.bgcolor}>{x}</li>
    </>
   )
 }
